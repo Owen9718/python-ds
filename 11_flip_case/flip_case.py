24e44c216637f7e4855871935e82598456c0199f
Owen9718 <96035029+Owen9718@users.noreply.github.com>
2022-03-22 22:40:23 -0400

    Add files via upload

---
+++ 11_flip_case/flip_case.py
@@ -0,0 +1,20 @@
+def flip_case(phrase, to_swap):
+    """Flip [to_swap] case each time it appears in phrase.
+
+        >>> flip_case('Aaaahhh', 'a')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'A')
+        'aAAAhhh'
+
+        >>> flip_case('Aaaahhh', 'h')
+        'AaaaHHH'
+
+    """
+    swap = to_swap.lower()
+    str = ''
+    for ltr in phrase:
+        if ltr.lower() == swap:
+            ltr = ltr.swapcase()
+        str += ltr
+    return str
---
+++ 14_compact/compact.py
@@ -0,0 +1,11 @@
+def compact(lst):
+    """Return a copy of lst with non-true elements removed.
+
+        >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
+        [1, 2, 'All done']
+    """
+    new_lst = []
+    for obj in lst:
+        if obj:
+            new_lst.append(obj)
+    return new_lst
